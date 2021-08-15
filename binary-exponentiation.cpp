b253a60125d25008e864dc12c94d37e6661a1bc5
izano007 <arindamrivu@gmail.com>
2021-08-15 13:41:51 +0530

    Fixed intendation

--- binary-exponentiation.cpp
+++ binary-exponentiation.cpp
@@ -1,24 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll; 
+typedef long long ll;
 
+const int M = 1e9 + 7;
 
-ll power(ll a,ll b)    //Avoid overflows by %M some given in problem statement
+ll power(ll a, ll b) //Avoid overflows by %M some given in problem statement
 {
-    ll result=1LL;
-    while(b>0)
+    ll result = 1LL;
+    while (b > 0)
     {
-        if(b%2==1) result*=a;
-        a*=a;
-        b/=2;
+        if (b % 2 == 1)
+            result *= a;
+        result = result % M;
+        a *= a;
+        a = a % M;
+        b /= 2;
     }
     return result;
 }
 
-
 int main()
 {
-    ll a,b;
-    cin>>a>>b;
-    cout<<power(a,b)<<endl;
+    ll a, b;
+    cin >> a >> b;
+    cout << power(a, b) << endl;
 }
--- edit-distance-str1-to-str2.cpp
+++ edit-distance-str1-to-str2.cpp
@@ -1,37 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int mxN=100;
-const int add_cost=1;//cost of addition
-const int del_cost=1;//cost of deletion
-const int trans_cost=1;//cost of transition
+const int mxN = 100;
+const int add_cost = 1;   //cost of addition
+const int del_cost = 1;   //cost of deletion
+const int trans_cost = 1; //cost of transition
 
 //costs can be taken from input
 
-
-int edit_dist(string& initial,string& final)
+int edit_dist(string &initial, string &final)
 {
-    vector<vector<int>> dp(mxN,vector<int>(mxN));
-    int m=initial.length();
-    int n=final.length();
-    for(int i=0;i<=n;i++) dp[i][0]=i;
-    for(int i=0;i<=m;i++) dp[0][i]=i;
-    for(int i=1;i<=n;i++)
+    vector<vector<int>> dp(mxN, vector<int>(mxN));
+    int m = initial.length();
+    int n = final.length();
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = i;
+    for (int i = 0; i <= m; i++)
+        dp[0][i] = i;
+    for (int i = 1; i <= n; i++)
     {
-        for(int j=1;j<=m;j++)
+        for (int j = 1; j <= m; j++)
         {
-            if(initial[j-1]==final[i-1]) dp[i][j]=dp[i-1][j-1];
-            else dp[i][j]=min(dp[i][j-1]+add_cost,min(dp[i-1][j]+del_cost,dp[i-1][j-1]+trans_cost));
+            if (initial[j - 1] == final[i - 1])
+                dp[i][j] = dp[i - 1][j - 1];
+            else
+                dp[i][j] = min(dp[i][j - 1] + add_cost, min(dp[i - 1][j] + del_cost, dp[i - 1][j - 1] + trans_cost));
         }
     }
     return dp[n][m];
 }
 
-
-
 int main()
 {
-    string initial,final;
-    cin>>initial>>final;
-    cout<<"Total cost: "<<edit_dist(initial,final)<<endl;
+    string initial, final;
+    cin >> initial >> final;
+    cout << "Total cost: " << edit_dist(initial, final) << endl;
 }
