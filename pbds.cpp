227a096d026ea14e26b8ffb8837c9b90aa160a52
Arindam Ray <57029184+Izano007@users.noreply.github.com>
2022-04-20 11:45:15 +0530

    Create pbds.cpp

---
+++ pbds.cpp
@@ -0,0 +1,18 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+	ordered_set<pair<int, int>> os;
+  os.insert({5, 1});
+  os.insert({6, 2});
+  os.insert({4, 3});
+  os.insert({1, 4});
+  cout << os.order_of_key({4,-1});  // values strictly less than 4(1);
+  cout << os.order_of_key({5,-1});  // values strictly less than 5(2);
+  cout << os.find_by_order(i)->first; // iterator of ith element;
+	return 0;
+}
