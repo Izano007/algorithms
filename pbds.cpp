c0d723d381d3e25792660e25ff1d4609edcf6084
Arindam Ray <57029184+Izano007@users.noreply.github.com>
2022-04-20 11:46:11 +0530

    Update pbds.cpp

--- pbds.cpp
+++ pbds.cpp
@@ -7,12 +7,12 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 int main()
 {
 	ordered_set<pair<int, int>> os;
-  os.insert({5, 1});
-  os.insert({6, 2});
-  os.insert({4, 3});
-  os.insert({1, 4});
-  cout << os.order_of_key({4,-1});  // values strictly less than 4(1);
-  cout << os.order_of_key({5,-1});  // values strictly less than 5(2);
-  cout << os.find_by_order(i)->first; // iterator of ith element;
+	os.insert({5, 1});
+	os.insert({6, 2});
+	os.insert({4, 3});
+	os.insert({1, 4});
+	cout << os.order_of_key({4,-1});  // values strictly less than 4(1);
+	cout << os.order_of_key({5,-1});  // values strictly less than 5(2);
+	cout << os.find_by_order(i)->first; // iterator of ith element;
 	return 0;
 }
